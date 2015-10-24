db474d504ee7324b181c217b25633072e1a46ef2
Lucas <lucas.helainemenard@hotmail.fr>
2015-10-24 14:08:58 -0400

    Discovery and Connection Feature

--- app/src/main/java/com/vibz/vibz/MenuActivity.java
+++ app/src/main/java/com/vibz/vibz/MenuActivity.java
@@ -4,22 +4,37 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.os.Handler;
 import android.widget.TextView;
 
+
 /**
  * Created by nicolasszewe on 23/10/15.
  */
 public class MenuActivity extends AppCompatActivity {
+
+    public static final String TAG = "wifidirectdemo";
+    private WifiP2pManager manager;
+    private boolean isWifiP2pEnabled = false;
+    private boolean retryChannel = false;
+
+
+
+    /**
+     * @param isWifiP2pEnabled the isWifiP2pEnabled to set
+     */
+    public void setIsWifiP2pEnabled(boolean isWifiP2pEnabled) {
+        this.isWifiP2pEnabled = isWifiP2pEnabled;
+    }
+
     public static MusicService musicSrv;
     public static SongAdapter songAdt;
     private Intent musicServiceIntent;
@@ -57,10 +72,13 @@ public class MenuActivity extends AppCompatActivity {
             bindService(musicServiceIntent, musicConnection, Context.BIND_AUTO_CREATE);
             startService(musicServiceIntent);
         }
+
+
     }
 
     Runnable run = new Runnable() {
-        @Override public void run() {
+        @Override
+        public void run() {
             seekUpdation();
         }
     };
@@ -74,7 +92,7 @@ public class MenuActivity extends AppCompatActivity {
         seek_bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 
             @Override
-            public void onProgressChanged(SeekBar arg0, int songProgress, boolean arg2){
+            public void onProgressChanged(SeekBar arg0, int songProgress, boolean arg2) {
             }
 
             @Override
@@ -87,7 +105,7 @@ public class MenuActivity extends AppCompatActivity {
                     int songProgress = seekBar.getProgress();
                     musicSrv.playSongAgain(songProgress);
                     song_progress_text.setText(convertPositionString(songProgress) + "/" + MusicService.PlaylistSongs.get(0).getStringDuration());
-                    Button p = (Button)findViewById(R.id.pause_play_song);
+                    Button p = (Button) findViewById(R.id.pause_play_song);
                     p.setText("| |");
                 }
             }
@@ -95,17 +113,16 @@ public class MenuActivity extends AppCompatActivity {
 
         });
 
-        if(MusicService.PlaylistSongs.size()>0) {
+        if (MusicService.PlaylistSongs.size() > 0) {
             seek_bar.setMax((int) MusicService.PlaylistSongs.get(0).getDuration());
-        }
-        else {
+        } else {
             seek_bar.setMax(0);
         }
         seekHandler.postDelayed(run, 1000);
     }
 
     public void seekUpdation() {
-        if(MusicService.isPlaying==true) {
+        if (MusicService.isPlaying == true) {
             seek_bar.setProgress(musicSrv.getPosn());
             song_progress_text.setText(convertPositionString(musicSrv.getPosn()) + "/" + MusicService.PlaylistSongs.get(0).getStringDuration());
         }
@@ -113,50 +130,49 @@ public class MenuActivity extends AppCompatActivity {
     }
 
 
-
-
-    public String convertPositionString(long position){
-        String seconds = String.valueOf((position  % 60000) / 1000);
+    public String convertPositionString(long position) {
+        String seconds = String.valueOf((position % 60000) / 1000);
         String minutes = String.valueOf(position / 60000);
         String stringDuration = minutes + ":" + seconds;
         return stringDuration;
     }
 
-    public void refreshPlaylist(){
+    public void refreshPlaylist() {
         itemView = (ListView) findViewById(R.id.playlist);
         this.songAdt = new SongAdapter(this, MusicService.PlaylistSongs);
         itemView.setAdapter(songAdt);
     }
 
-    public void onPausePlaySong(View view){
-        if(musicSrv.isPlaying==true){
+    public void onPausePlaySong(View view) {
+        if (musicSrv.isPlaying == true) {
             musicSrv.pauseSong();
             musicSrv.isPlaying = false;
-            Button p = (Button)findViewById(R.id.pause_play_song);
+            Button p = (Button) findViewById(R.id.pause_play_song);
             p.setText(">");
 
-        }
-        else if(MusicService.PlaylistSongs.size()>0){
+        } else if (MusicService.PlaylistSongs.size() > 0) {
             musicSrv.playSongAgain();
             MusicService.isPlaying = true;
-            Button p = (Button)findViewById(R.id.pause_play_song);
+            Button p = (Button) findViewById(R.id.pause_play_song);
             p.setText("| |");
         }
     }
 
-    public void onNextSong(View view){
-       musicSrv.nextSong();
+    public void onNextSong(View view) {
+        musicSrv.nextSong();
     }
 
     public void CheckConnection(View view) {
-        //Intent intent = new Intent(this, Connection.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, WiFiDirectActivity.class);
+        startActivity(intent);
     }
 
     public void AddSongButton(View view) {
         Intent intent = new Intent(this, ListCategoryActivity.class);
         startActivity(intent);
     }
+
+
 
     @Override
     protected void onDestroy() {
